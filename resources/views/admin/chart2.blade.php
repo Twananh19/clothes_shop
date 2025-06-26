300b91874d4ce247a970aab67803186789bb2bc4
Twananh19 <vtanhh20@example.com>
2025-06-26 19:20:18 +0700

    create admin page

---
+++ resources/views/admin/chart2.blade.php
@@ -0,0 +1,197 @@
+<div class="grid grid-cols-1 lg:grid-cols-3 lg:gap-x-6 gap-x-0 lg:gap-y-0 gap-y-6">
+                          <div class="card">
+							<div class="card-body">
+							<h4 class="text-gray-500 text-lg font-semibold mb-5">Upcoming Schedules</h4>
+							<ul class="timeline-widget relative">
+                               <li class="timeline-item flex relative overflow-hidden min-h-[70px]">
+                                   <div class="timeline-time text-gray-500 text-sm min-w-[90px] py-[6px] pr-4 text-end">
+                                       9:30 am
+								   </div>
+								   <div class="timeline-badge-wrap flex flex-col items-center ">
+                                     <div class="timeline-badge w-3 h-3 rounded-full shrink-0 bg-transparent border-2 border-blue-600 my-[10px]">
+									 </div>
+									 <div class="timeline-badge-border block h-full w-[1px] bg-gray-100"></div>
+								   </div>
+								   <div class="timeline-desc py-[6px] px-4">
+									<p class="text-gray-500 text-sm font-normal">Payment received from John Doe of $385.90</p>
+								   </div>
+							   </li>
+                               <li class="timeline-item flex relative overflow-hidden min-h-[70px]">
+                                   <div class="timeline-time text-gray-500 min-w-[90px] py-[6px] text-sm pr-4 text-end">
+                                       10:00 am
+								   </div>
+								   <div class="timeline-badge-wrap flex flex-col items-center ">
+                                     <div class="timeline-badge w-3 h-3 rounded-full shrink-0 bg-transparent border-2 border-blue-300 my-[10px]">
+									 </div>
+									 <div class="timeline-badge-border block h-full w-[1px] bg-gray-100"></div>
+								   </div>
+								   <div class="timeline-desc py-[6px] px-4 text-sm">
+									<p class="text-gray-500  font-semibold">New sale recorded</p>
+									<a href="javascript:void('')" class="text-blue-600">#ML-3467</a>
+								   </div>
+							   </li>
+
+                               <li class="timeline-item flex relative overflow-hidden min-h-[70px]">
+								<div class="timeline-time text-gray-500 min-w-[90px] text-sm py-[6px] pr-4 text-end">
+									12:00 am
+								</div>
+								<div class="timeline-badge-wrap flex flex-col items-center ">
+								  <div class="timeline-badge w-3 h-3 rounded-full shrink-0 bg-transparent border-2 border-teal-500 my-[10px]">
+								  </div>
+								  <div class="timeline-badge-border block h-full w-[1px] bg-gray-100"></div>
+								</div>
+								<div class="timeline-desc py-[6px] px-4">
+								 <p class="text-gray-500 text-sm font-normal">Payment was made of $64.95 to Michael</p>
+								</div>
+							</li>
+							
+							<li class="timeline-item flex relative overflow-hidden min-h-[70px]">
+								<div class="timeline-time text-gray-500 min-w-[90px] text-sm py-[6px] pr-4 text-end">
+									9:30 am
+								</div>
+								<div class="timeline-badge-wrap flex flex-col items-center ">
+								  <div class="timeline-badge w-3 h-3 rounded-full shrink-0 bg-transparent border-2 border-yellow-500 my-[10px]">
+								  </div>
+								  <div class="timeline-badge-border block h-full w-[1px] bg-gray-100"></div>
+								</div>
+								<div class="timeline-desc py-[6px] px-4 text-sm">
+								 <p class="text-gray-500 font-semibold">New sale recorded</p>
+								 <a href="javascript:void('')" class="text-blue-600">#ML-3467</a>
+								</div>
+							</li>
+
+							<li class="timeline-item flex relative overflow-hidden min-h-[70px]">
+								<div class="timeline-time text-gray-500 text-sm min-w-[90px] py-[6px] pr-4 text-end">
+									9:30 am
+								</div>
+								<div class="timeline-badge-wrap flex flex-col items-center ">
+								  <div class="timeline-badge w-3 h-3 rounded-full shrink-0 bg-transparent border-2 border-red-500 my-[10px]">
+								  </div>
+								  <div class="timeline-badge-border block h-full w-[1px] bg-gray-100"></div>
+								</div>
+								<div class="timeline-desc py-[6px] px-4">
+								 <p class="text-gray-500 text-sm font-semibold">New arrival recorded</p>
+								</div>
+							</li>
+							<li class="timeline-item flex relative overflow-hidden">
+								<div class="timeline-time text-gray-500 text-sm min-w-[90px] py-[6px] pr-4 text-end">
+									12:00 am
+								</div>
+								<div class="timeline-badge-wrap flex flex-col items-center ">
+								  <div class="timeline-badge w-3 h-3 rounded-full shrink-0 bg-transparent border-2 border-teal-500 my-[10px]">
+								  </div>
+								  <div class="timeline-badge-border block h-full w-[1px] bg-gray-100"></div>
+								</div>
+								<div class="timeline-desc py-[6px] px-4">
+								 <p class="text-gray-500 text-sm font-normal">Payment Done</p>
+								</div>
+							</li>							
+							   
+							</ul>
+							</div>
+						  </div>
+						  <div class="col-span-2">
+							<div class="card h-full">
+								<div class="card-body">
+									<h4 class="text-gray-500 text-lg font-semibold mb-5">Top Paying Clients</h4>
+									<div class="relative overflow-x-auto">
+										<!-- table -->
+										<table class="text-left w-full whitespace-nowrap text-sm text-gray-500">
+											<thead >
+												<tr class="text-sm">
+													<th scope="col" class="p-4 font-semibold">Profile</th>
+													<th scope="col" class="p-4 font-semibold">Hour Rate</th>
+													<th scope="col" class="p-4 font-semibold">Extra classes</th>
+													<th scope="col" class="p-4 font-semibold">Status</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td class="p-4 text-sm">
+                                                       <div class="flex gap-6 items-center">
+														<div class="h-12 w-12 inline-block"><img src="admin/assets/images/profile/user-1.jpg" alt="" class="rounded-full w-100"></div>
+														<div class="flex flex-col gap-1 text-gray-500">
+														  <h3 class=" font-bold">Mark J. Freeman</h3>
+														  <span class="font-normal">Prof. English</span>
+													  </div>
+													   </div>
+													</td>
+													<td class="p-4">
+                                                         <h3 class="font-medium">$150/hour</h3>
+													</td>
+													<td class="p-4">
+														<h3 class="font-medium text-teal-500">+53</h3>
+													</td>
+													<td class="p-4">
+														<span class="inline-flex items-center py-2 px-4 rounded-3xl font-semibold bg-teal-400 text-teal-500">Available</span>
+													</td>
+												</tr>
+												<tr>
+													<td class="p-4 text-sm">
+                                                       <div class="flex gap-6 items-center">
+														<div class="h-12 w-12 inline-block"><img src="admin/assets/images/profile/user-2.jpg" alt="" class="rounded-full w-100"></div>
+														<div class="flex flex-col gap-1 text-gray-500">
+														  <h3 class=" font-bold">Nina R. Oldman</h3>
+														  <span class="font-normal">Prof. History</span>
+													  </div>
+													   </div>
+													</td>
+													<td class="p-4">
+                                                         <h3 class="font-medium">$150/hour</h3>
+													</td>
+													<td class="p-4">
+														<h3 class="font-medium text-teal-500">+68</h3>
+													</td>
+													<td class="p-4">
+														<span class="inline-flex items-center py-2 px-4 rounded-3xl font-semibold bg-blue-500 text-blue-600">In Class</span>
+													</td>
+												</tr>
+												<tr>
+													<td class="p-4 text-sm">
+                                                       <div class="flex gap-6 items-center">
+														<div class="h-12 w-12 inline-block"><img src="admin/assets/images/profile/user-3.jpg" alt="" class="rounded-full w-100"></div>
+														<div class="flex flex-col gap-1 text-gray-500">
+														  <h3 class=" font-bold">Arya H. Shah</h3>
+														  <span class="font-normal">Prof. Maths</span>
+													  </div>
+													   </div>
+													</td>
+													<td class="p-4">
+                                                         <h3 class="font-medium">$150/hour</h3>
+													</td>
+													<td class="p-4">
+														<h3 class="font-medium text-teal-500">+94</h3>
+													</td>
+													<td class="p-4">
+														<span class="inline-flex items-center py-2 px-4 rounded-3xl font-semibold bg-red-400 text-red-500">Absent</span>
+													</td>
+												</tr>
+												<tr>
+													<td class="p-4 text-sm">
+                                                       <div class="flex gap-6 items-center">
+														<div class="h-12 w-12 inline-block"><img src="admin/assets/images/profile/user-4.jpg" alt="" class="rounded-full w-100"></div>
+														<div class="flex flex-col gap-1 text-gray-500">
+														  <h3 class=" font-bold">June R. Smith</h3>
+														  <span class="font-normal">Prof. Arts</span>
+													  </div>
+													   </div>
+													</td>
+													<td class="p-4">
+                                                         <h3 class="font-medium">$150/hour</h3>
+													</td>
+													<td class="p-4">
+														<h3 class="font-medium text-teal-500">+27</h3>
+													</td>
+													<td class="p-4">
+														<span class="inline-flex items-center py-2 px-4 rounded-3xl font-semibold bg-yellow-400 text-yellow-500">Absent</span>
+													</td>
+												</tr>
+
+
+											</tbody>
+										</table>
+									</div>									
+								</div>
+							</div>
+						  </div>
+					   </div>
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,14 +1,102 @@
-<x-app-layout>
-    
-</x-app-layout>
 <!DOCTYPE html>
-<html lang="en">
+<html   lang="en" >
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+	<!-- Required meta tags -->
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<!-- Favicon icon-->
+<link rel="shortcut icon" type="image/png" href="admin/assets/images/logos/favicon.png" />
+<link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap"
+  rel="stylesheet" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@2.44.0/tabler-icons.min.css">
+<!-- Core Css -->
+<link rel="stylesheet" href="admin/assets/css/theme.css" />
+	<title>Spike TailwindCSS HTML Admin Template</title>
 </head>
-<body>
-    <h1>Welcome to the Admin Home Page</h1>
+
+<body class=" bg-surface">
+	<main>
+		<div class="app-topstrip z-40 sticky top-0 py-[15px] px-6 bg-[linear-gradient(90deg,_#0f0533_0%,_#1b0a5c_100%)]">
+			<div class="flex flex-col lg:flex-row gap-4 items-center justify-between">
+                <div class="md:flex hidden items-center gap-5">
+                    <a href="">
+                        <img src="admin/assets/images/logos/logo-wrappixel.svg" width="147" alt="logo-wrappixel" />
+                    </a>
+                </div>
+                <div class="flex flex-col md:flex-row items-center gap-4 justify-center">
+                    <h4 class="text-sm font-normal text-white uppercase font-semibold bg-[linear-gradient(90deg,_#FFFFFF_0%,_#8D70F8_100%)] [-webkit-background-clip:text] [background-clip:text] [-webkit-text-fill-color:transparent]">Checkout Pro Version</h4>
+                </div>
+            </div>
+		</div>
+		<!--start the project-->
+		<div id="main-wrapper" class="flex p-5 xl:pr-0">
+			<aside id="application-sidebar-brand"
+				class="hs-overlay hs-overlay-open:translate-x-0 -translate-x-full  transform hidden xl:block xl:translate-x-0 xl:end-auto xl:bottom-0 fixed xl:top-[90px] xl:left-auto top-0 left-0 with-vertical h-screen z-[999] shrink-0  w-[270px] shadow-md xl:rounded-md rounded-none bg-white left-sidebar   transition-all duration-300" >
+				<!-- ---------------------------------- -->
+<!-- Start Vertical Layout Sidebar -->
+<!-- ---------------------------------- -->
+@include('admin.sidebar')
+
+                <!-- ---------------------------------- -->
+
+			</aside>
+			<div class=" w-full page-wrapper xl:px-6 px-0">
+
+				<!-- Main Content -->
+				<main class="h-full  max-w-full">
+					<div class="container full-container p-0 flex flex-col gap-6">
+					<!--  Header Start -->
+				<header class=" bg-white shadow-md rounded-md w-full text-sm py-4 px-6">
+					
+
+<!-- ========== HEADER ========== -->
+
+    @include('admin.navigation-menu')
+
+
+  <!-- ========== END HEADER ========== -->
+				</header>
+				<!--  Header End -->
+                      @include('admin.chart')
+
+
+					  </div>
+                       @include('admin.chart2')
+					   @include('admin.table')
+					   <footer>
+						<p class="text-base text-gray-400 font-normal p-3 text-center">
+							Design and Developed by <a href="https://www.wrappixel.com/"   class="text-blue-600 underline hover:text-blue-700">wrappixel.com</a>. Distributed by <a href="https://themewagon.com"  class="text-blue-600 underline hover:text-blue-700" target="_blank">ThemeWagon</a>
+						</p>
+					   </footer>
+					</div>
+
+
+				</main>
+				<!-- Main Content End -->
+				
+			</div>
+		</div>
+		<!--end of project-->
+	</main>
+
+
+	
+<script src="admin/assets/libs/jquery/dist/jquery.min.js"></script>
+<script src="admin/assets/libs/simplebar/dist/simplebar.min.js"></script>
+<script src="admin/assets/libs/iconify-icon/dist/iconify-icon.min.js"></script>
+<script src="admin/assets/libs/@preline/dropdown/index.js"></script>
+<script src="admin/assets/libs/@preline/overlay/index.js"></script>
+<script src="admin/assets/js/sidebarmenu.js"></script>
+
+<!-- solar icons -->
+<script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js"></script>
+<script src="admin/assets/libs/preline/dist/preline.js"></script>
+
+	<script src="admin/assets/libs/apexcharts/dist/apexcharts.min.js"></script>
+    <script src="admin/assets/js/dashboard.js"></script>
 </body>
+
 </html>
